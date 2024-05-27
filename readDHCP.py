d17d854669f9056d9e0ab0fa8fad6bd0cd746f86
Yingxiang Yao <yingxiang.yao@kasumi321.de>
2024-05-27 22:49:38 +0200

    readDHCP sorts IP list

--- readDHCP.py
+++ readDHCP.py
@@ -1,11 +1,16 @@
+import socket
+
+
 file = open('/etc/dhcp/dhcpd.conf', 'r')
 lines = file.readlines()
 position = 0
 header = ['Hostname','MAC','IP']
 format_string = "{: >20} {: >25} {: >15}"
-info_string = "IP Range: \n 0...10: Infrastructural Devices and VMs \n 11...20: Network Devices \n 21...40: Persistent VMs \n 41...50: Desktop and Laptop PCs \n 51...60: Mobil>print(info_string)
+info_string = "IP Range: \n 0...10: Infrastructural Devices and VMs \n 11...20: Network Devices \n 21...40: Persistent VMs \n 41...50: Desktop and Laptop PCs \n 51...60: Mobil "
+print(info_string)
 print(format_string.format(*header))
 print("=================================================================")
+ip_entry = []
 
 for line in lines:
         position = position + 1
@@ -15,10 +20,13 @@ for line in lines:
                 mac_address = lines[position].replace("  hardware ethernet", ' ').replace(";",' ').strip()
                 ip_address = lines[position+1].replace(" fixed-address", ' ').replace(";",' ').strip()
                 entry = [host_name, mac_address, ip_address]
-                print(format_string.format(*entry))
+                ip_entry.append(entry)
+                #print(format_string.format(*entry))
 
 
+ip_entry_sorted = sorted(ip_entry, key=lambda item: socket.inet_aton(item[2]) )
 
-
+for i in range(len(ip_entry_sorted)):
+    print(format_string.format(*ip_entry_sorted[i]))
 
 file.close()
